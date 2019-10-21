bcc8a7250958361a7dc3e7159f34bacff1f0323d
yanxilin <yanxi853600@gmail.com>
2019-10-21 20:27:20 +0800

    add repo

---
+++ RForest.py
@@ -0,0 +1,30 @@
+#random forest
+
+import pandas as pd
+from sklearn.cross_validation import train_test_split
+from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
+
+
+avocado=pd.read_csv("avocado.csv")
+print(avocado.head())
+
+x=pd.DataFrame([avocado['Total Volume'],
+                avocado['Total Bags'],
+                avocado['AveragePrice'],
+                avocado['Small Bags'],
+                avocado['Large Bags'],
+                avocado['XLarge Bags']]).T
+y=avocado['type']
+
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.25,random_state=0)
+
+cl = tree.DecisionTreeClassifier(max_depth = 3)
+clf=cl.fit(x_train,y_train)
+
+randomforest=RandomForestClassifier()
+randomforest.fit(x_train,y_train)
+predicted=randomforest.predict(x_test)
+print(randomforest)
+
+print("Accuracy:",clf.score(x_test,y_test))
